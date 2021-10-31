b824c9c80d7a26b7a1f83d8d1bd56af8031d27a6
startdusk <522383345@qq.com>
2021-10-31 22:30:06 +0800

    Added real Command data to DB

---
+++ src/CommandAPI/Migrations/20211031142410_AddCommandsData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CommandAPI.Migrations
+{
+    public partial class AddCommandsData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // ref: https://stackoverflow.com/questions/52382691/add-data-using-migration-entity-framework-core-without-specifying-the-id
+            migrationBuilder.InsertData(
+                "command", // tableName
+            new string[] { // columns
+                "how_to",
+                "platform",
+                "command_line"
+            },
+            new object[]{ // values
+                "Create an EF migration",
+                "Entity Framework Core Command Line",
+                "dotnet ef migrations add"
+            });
+            migrationBuilder.InsertData(
+                "command", // tableName
+            new string[] { // columns
+                "how_to",
+                "platform",
+                "command_line"
+            },
+            new object[]{ // values
+                "Apply Migrations to DB",
+                "Entity Framework Core Command Line",
+                "dotnet ef database update"
+            });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
